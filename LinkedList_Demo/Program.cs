a523c1e0f254d242cababdd9850789a6effb2c90
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-17 22:47:54 +0530

    [chiru] Checking Condition 56->30->70

--- LinkedList_Demo/Program.cs
+++ LinkedList_Demo/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.Add(30);
+            list.Add(56);
             list.Display();
         }
     }
