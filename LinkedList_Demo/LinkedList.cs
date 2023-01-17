1b94b3fcea1f02f17de685a3cb72a298990d62e4
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-17 22:42:37 +0530

    [chiru] Create a LinkedList

---
+++ LinkedList_Demo/LinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList_Demo
+{
+    public class LinkedList
+    {
+        public Node head;//new
+        public void Add(int data)//method
+        {
+            Node n = new Node(data);
+            if (this.head == null)
+            {
+                this.head = n;
+            }
+
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = n;
+            }
+            Console.WriteLine("{0} inserted into the linked list", n.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+        }
+    }
+}
