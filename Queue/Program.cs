42e6bba807eaffe8130d7fcd87d10f33589980c8
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-18 11:20:53 +0530

    [chiru] Perform Enqueue Operation In Queue

---
+++ Queue/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Queue Problem!");
+            QueueList linkedListQueue = new QueueList();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
+            Console.ReadKey();
+        }
+    }
+}
