7a629cb36218abde3888dbee497445b727e527de
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-18 11:00:07 +0530

    [chiru] Creating stack

---
+++ Stack/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Stack Problems");
+            StackLink stacklinked = new StackLink();
+            stacklinked.LinkedListStack();
+            stacklinked.Push(70);
+            stacklinked.Push(30);
+            stacklinked.Push(56);
+        }
+    }
+}
---
+++ Stack/StackLink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class StackLink
+    {
+        private Node top;
+        public static int a = 10;
+        public void LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+    }
+}
